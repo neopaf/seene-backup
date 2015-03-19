613b32e432f47950f65c5700b5eb12dd5208b68a
Alexander Petrossian (PAF@home) <alexander.petrossian@teligent.ru>
2015-03-20 02:10:30 +0300

    initial

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # seene-backup
 auto-backup of http://seene.co/ data
+
+Usage:
+./seene-backup your_user_nick
+Creates folder with current date and all your seenes.
+
+Each seene is stored in a folder named 
+<captured_at> <caption> (#hashtags, if any)
+
+Prerequisity:
+http://stedolan.github.io/jq/
+in your path.
+
+Tested on my Mac.
+Should be easily ported to Windows.
---
+++ seene-backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+user=$1
+date=$(date +%Y-%m-%d)
+id=$(curl -s http://seene.co/api/seene/-/users/@$user|jq .id)
+curl -s http://seene.co/api/seene/-/users/$id/scenes?count=10000 > scenes.json
+
+cat scenes.json|jq -c -r '.scenes[] | .captured_at+" "+.caption+if .links|length>0 then " ("+([.links | .[] | .target] | join(" "))+")" else "" end + "\t" + .poster_url + "\t" + .model_url'>scenes.xls
+
+cat scenes.xls |while IFS=$'\t' read -r title poster_url model_url
+do
+	echo "$title"
+	curl -sR --create-dirs $poster_url -o $date/"$title"/poster.jpg
+	curl -sR --create-dirs $model_url -o $date/"$title"/scene.oemodel
+done
